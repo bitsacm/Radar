a96efce25197cf3735ff7b3197f76503704baf7e
Penguin-12 <josh.wadhwa@live.com>
2020-07-06 15:27:06 +0530

    Added auto login.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
+import 'package:Radar/HomeScreen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
+import 'auth/model/LoginScreenController.dart';
 import 'auth/view/LoginScreen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
+  if ((await _secureStorage.read(key: 'UID')) == null)
+    runApp(MyApp(
+      initialRoute: '/login',
+      secureStorage: _secureStorage,
+    ));
+  else
+    runApp(MyApp(
+      initialRoute: '/',
+      secureStorage: _secureStorage,
+    ));
 }
 
 class MyApp extends StatelessWidget {
+  final FlutterSecureStorage secureStorage;
+  final String initialRoute;
+  MyApp({@required this.secureStorage, @required this.initialRoute});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +33,17 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginScreen(),
+      initialRoute: initialRoute,
+      routes: {
+        '/login': (context) {
+          return ChangeNotifierProvider<LoginScreenController>(
+              create: (context) => LoginScreenController(),
+              child: LoginScreen(secureStorage));
+        },
+        '/': (context) {
+          return HomeScreen();
+        },
+      },
     );
   }
 }
