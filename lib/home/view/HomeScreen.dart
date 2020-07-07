48ea83501631ee4e100cba7befb4f5d4adcd78f2
Prarabdh Garg <prarabdhgrg@gmail.com>
2020-07-08 01:43:44 +0530

    Add bottom Navigation Bar to Home Screen

--- lib/home/view/HomeScreen.dart
+++ lib/home/view/HomeScreen.dart
@@ -1,18 +1,48 @@
 import 'package:Radar/home/model/HomeScreenController.dart';
+import 'package:Radar/home/view/ProfileScreen.dart';
+import 'package:Radar/home/view/RequestsScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex;
+  List<Widget> _pages = [
+    RequestsScreen(),
+    ProfileScreen()
+  ];
+
+  @override
+  void initState() {
+    this.currentIndex = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Consumer<HomeScreenController>(
-          builder: (context, controller, _) {
-            return Text(controller.locationData?.latitude.toString());
-          },
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            this.currentIndex = index;
+          });
+        },
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text('Requests')
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile')
+          )
+        ],
       ),
+      body: _pages[this.currentIndex],
     );
   }
 }
