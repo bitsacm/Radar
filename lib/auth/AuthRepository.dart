c91979046fcddd789fe908845fe132eed1d86ac8
Addepalli N M PavanKalyan <57014239+pavankalyan0424@users.noreply.github.com>
2020-07-28 20:09:17 +0530

    'auth_changes'

--- lib/auth/AuthRepository.dart
+++ lib/auth/AuthRepository.dart
@@ -18,15 +18,14 @@ class AuthRepository {
 
   Future<FirebaseUser> signInUsingGoogle() async {
     var googleUser = await _googleSignIn.signIn();
-    if (googleUser.email.endsWith('@pilani.bits-pilani.ac.in')) {
+    try {
       var googleSignInAuthentication = await googleUser.authentication;
       final AuthCredential credential = GoogleAuthProvider.getCredential(
           idToken: googleSignInAuthentication.idToken,
           accessToken: googleSignInAuthentication.accessToken);
       return (await _auth.signInWithCredential(credential)).user;
-    } else {
-      await _googleSignIn.signOut();
-      throw Failure('Please sign in with BITS Mail');
+    } catch(e) {
+      throw Failure('Unable to Login. Please try again.');
     }
   }
 }
