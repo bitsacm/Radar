821d5649611b21e01528ced28eb16664af79e98e
Addepalli N M PavanKalyan <57014239+pavankalyan0424@users.noreply.github.com>
2020-08-24 00:02:06 +0530

    Added Logout feature (#25)
    * Updates
    
    * Logout feature updates
    
    * Updates
    
    * update
    
    * Updates
    
    * Updated code as requeted

--- lib/profile/controller/ProfileController.dart
+++ lib/profile/controller/ProfileController.dart
@@ -1,15 +1,30 @@
 import 'package:Radar/utils/Roles.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nearby_connections/nearby_connections.dart';
 
 class ProfileController with ChangeNotifier {
   final Nearby _nearby = Nearby();
   final Roles roles;
-  ProfileController(this.roles);
+  final FlutterSecureStorage secureStorage;
+
+  ProfileController(this.roles, this.secureStorage);
 
   void cancelMyRequest() async {
     roles.requestCreater.clearRequestDetails();
     await _nearby.stopAdvertising();
     notifyListeners();
+  }
+
+  Future logout() async {
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    final _auth = FirebaseAuth.instance;
+    await secureStorage.delete(key: 'UID');
+    await secureStorage.delete(key: 'UserName');
+    await secureStorage.delete(key: 'Avatar');
+    await _auth.signOut();
+    await _googleSignIn.signOut();
   }
 }
--- lib/profile/view/ProfileScreen.dart
+++ lib/profile/view/ProfileScreen.dart
@@ -2,10 +2,10 @@ import 'package:Radar/profile/controller/ProfileController.dart';
 import 'package:Radar/profile/view/MyRequestBar.dart';
 import 'package:Radar/profile/view/MyRequestDetails.dart';
 import 'package:Radar/requests/controller/RequestsController.dart';
+import 'package:Radar/utils/ConnectionState.dart' as util;
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
-import 'package:Radar/utils/ConnectionState.dart' as util;
 
 class ProfileScreen extends StatelessWidget {
   final TextEditingController nameController = TextEditingController();
@@ -13,122 +13,125 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-        backgroundColor: Color.fromARGB(255, 22, 86, 189),
-        actions: <Widget>[
-          IconButton(
-            icon: Image.asset('assets/profile_screen/logout.png'),
-            onPressed: null,
-            color: Colors.white,
-          )
-        ],
-      ),
-      body: Consumer2<ProfileController, RequestsController>(
+    return Consumer2<ProfileController, RequestsController>(
         builder: (context, _controller, _, child) {
-          if (_controller.roles.requestCreater.connectionState ==
-                  util.ConnectionState.Disconnected ||
-              _controller.roles.requestCreater.connectionState ==
-                  util.ConnectionState.Connected) {
-            return Stack(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    vertical: mediaQuery.size.height > 400 ? 40 : 0,
-                  ),
-                  alignment: Alignment.topCenter,
-                  child: Column(
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundImage:
-                            AssetImage('assets/profile_screen/profile_pic.png'),
-                        radius: 50,
-                        backgroundColor: Colors.transparent,
-                      ),
-                      // ),
-                      Container(
-                        padding: EdgeInsets.all(12),
-                        child: ListTile(
-                          // trailing: IconButton(
-                          // icon: Image.asset(
-                          //     'assets/profile_screen/edit_name.png'),
-                          // onPressed: null),
-                          title: TextFormField(
-                            controller: nameController,
-                            decoration: InputDecoration(
-                              // icon: Icon(Icons.person),
-                              // labelText: 'Name ',
+      if (_controller.roles.requestCreater.connectionState ==
+              util.ConnectionState.Disconnected ||
+          _controller.roles.requestCreater.connectionState ==
+              util.ConnectionState.Connected) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Profile'),
+            backgroundColor: Color.fromARGB(255, 22, 86, 189),
+            actions: <Widget>[
+              IconButton(
+                icon: Image.asset('assets/profile_screen/logout.png'),
+                onPressed: () async {
+                  await _controller.logout();
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, '/login', (route) => false);
+                },
+                color: Colors.white,
+              )
+            ],
+          ),
+          body: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: mediaQuery.size.height > 400 ? 40 : 0,
+                ),
+                alignment: Alignment.topCenter,
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/profile_screen/profile_pic.png'),
+                      radius: 50,
+                      backgroundColor: Colors.transparent,
+                    ),
+                    // ),
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      child: ListTile(
+                        // trailing: IconButton(
+                        // icon: Image.asset(
+                        //     'assets/profile_screen/edit_name.png'),
+                        // onPressed: null),
+                        title: TextFormField(
+                          controller: nameController,
+                          decoration: InputDecoration(
+                            // icon: Icon(Icons.person),
+                            // labelText: 'Name ',
 
-                              fillColor: Colors.white,
-                              focusColor: Colors.white,
-                              hintText: 'Full Name',
-                              hintStyle: TextStyle(
-                                fontFamily: 'Verdana',
-                                fontSize: 16,
-                              ),
-                              suffixIcon: Padding(
-                                padding: EdgeInsets.all(10),
-                                child: SvgPicture.asset(
-                                  'assets/profile_screen/edit_name.svg',
-                                ),
-                              ),
-
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(9),
-                                borderSide:
-                                    BorderSide(color: Colors.grey, width: 0.0),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(9),
-                                borderSide:
-                                    BorderSide(color: Colors.grey, width: 0.0),
+                            fillColor: Colors.white,
+                            focusColor: Colors.white,
+                            hintText: 'Full Name',
+                            hintStyle: TextStyle(
+                              fontFamily: 'Verdana',
+                              fontSize: 16,
+                            ),
+                            suffixIcon: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: SvgPicture.asset(
+                                'assets/profile_screen/edit_name.svg',
                               ),
                             ),
-                            onEditingComplete: null,
-                            validator: (String value) {
-                              if (value.isEmpty) {
-                                return "";
-                              }
-                              return null;
-                            },
+
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(9),
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 0.0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(9),
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 0.0),
+                            ),
                           ),
+                          onEditingComplete: null,
+                          validator: (String value) {
+                            if (value.isEmpty) {
+                              return "";
+                            }
+                            return null;
+                          },
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                if (_controller.roles.requestCreater.doRequestDetailsExist())
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: InkWell(
-                      child: MyRequestBar(
-                        myRequestTitle:
-                            _controller.roles.requestCreater.requestTitle,
-                        cancelMyRequest: _controller.cancelMyRequest,
-                      ),
-                      onTap: () => showModalBottomSheet(
-                        builder: (context) => MyRequestDetails(
-                          myRequestTitle:
-                              _controller.roles.requestCreater.requestTitle,
-                          myRequestDescription: _controller
-                              .roles.requestCreater.requestDescription,
-                        ),
-                        context: context,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0)),
-                        backgroundColor: Colors.white,
                       ),
                     ),
-                  )
-              ],
-            );
-          } else
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-        },
-      ),
-    );
+                  ],
+                ),
+              ),
+              if (_controller.roles.requestCreater.doRequestDetailsExist())
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: InkWell(
+                    child: MyRequestBar(
+                      myRequestTitle:
+                          _controller.roles.requestCreater.requestTitle,
+                      cancelMyRequest: _controller.cancelMyRequest,
+                    ),
+                    onTap: () => showModalBottomSheet(
+                      builder: (context) => MyRequestDetails(
+                        myRequestTitle:
+                            _controller.roles.requestCreater.requestTitle,
+                        myRequestDescription:
+                            _controller.roles.requestCreater.requestDescription,
+                      ),
+                      context: context,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      backgroundColor: Colors.white,
+                    ),
+                  ),
+                )
+            ],
+          ),
+        );
+      } else
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+    });
   }
 }
