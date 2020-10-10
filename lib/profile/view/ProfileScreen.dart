a7225bb7c4bfc84b5665890264fde754f838876f
maplerichie <babierichie@hotmail.com>
2020-10-10 21:47:08 +0800

    Add logout confirmation dialog

--- lib/profile/view/ProfileScreen.dart
+++ lib/profile/view/ProfileScreen.dart
@@ -3,6 +3,7 @@ import 'package:Radar/profile/view/MyRequestBar.dart';
 import 'package:Radar/profile/view/MyRequestDetails.dart';
 import 'package:Radar/requests/controller/RequestsController.dart';
 import 'package:Radar/utils/ConnectionState.dart' as util;
+import 'package:Radar/utils/LogoutDialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
@@ -13,12 +14,9 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    return Consumer2<ProfileController, RequestsController>(
-        builder: (context, _controller, _, child) {
-      if (_controller.roles.requestCreater.connectionState ==
-              util.ConnectionState.Disconnected ||
-          _controller.roles.requestCreater.connectionState ==
-              util.ConnectionState.Connected) {
+    return Consumer2<ProfileController, RequestsController>(builder: (context, _controller, _, child) {
+      if (_controller.roles.requestCreater.connectionState == util.ConnectionState.Disconnected ||
+          _controller.roles.requestCreater.connectionState == util.ConnectionState.Connected) {
         return Scaffold(
           appBar: AppBar(
             title: Text('Profile'),
@@ -27,9 +25,15 @@ class ProfileScreen extends StatelessWidget {
               IconButton(
                 icon: Image.asset('assets/profile_screen/logout.png'),
                 onPressed: () async {
-                  await _controller.logout();
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, '/login', (route) => false);
+                  showDialog(
+                    context: context,
+                    builder: (context) => LogoutDialog(),
+                  ).then((val) async {
+                    if (val != null) {
+                      await _controller.logout();
+                      Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+                    }
+                  });
                 },
                 color: Colors.white,
               )
@@ -45,8 +49,7 @@ class ProfileScreen extends StatelessWidget {
                 child: Column(
                   children: <Widget>[
                     CircleAvatar(
-                      backgroundImage:
-                          AssetImage('assets/profile_screen/profile_pic.png'),
+                      backgroundImage: AssetImage('assets/profile_screen/profile_pic.png'),
                       radius: 50,
                       backgroundColor: Colors.transparent,
                     ),
@@ -80,13 +83,11 @@ class ProfileScreen extends StatelessWidget {
 
                             focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(9),
-                              borderSide:
-                                  BorderSide(color: Colors.grey, width: 0.0),
+                              borderSide: BorderSide(color: Colors.grey, width: 0.0),
                             ),
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(9),
-                              borderSide:
-                                  BorderSide(color: Colors.grey, width: 0.0),
+                              borderSide: BorderSide(color: Colors.grey, width: 0.0),
                             ),
                           ),
                           onEditingComplete: null,
@@ -107,20 +108,16 @@ class ProfileScreen extends StatelessWidget {
                   alignment: Alignment.bottomCenter,
                   child: InkWell(
                     child: MyRequestBar(
-                      myRequestTitle:
-                          _controller.roles.requestCreater.requestTitle,
+                      myRequestTitle: _controller.roles.requestCreater.requestTitle,
                       cancelMyRequest: _controller.cancelMyRequest,
                     ),
                     onTap: () => showModalBottomSheet(
                       builder: (context) => MyRequestDetails(
-                        myRequestTitle:
-                            _controller.roles.requestCreater.requestTitle,
-                        myRequestDescription:
-                            _controller.roles.requestCreater.requestDescription,
+                        myRequestTitle: _controller.roles.requestCreater.requestTitle,
+                        myRequestDescription: _controller.roles.requestCreater.requestDescription,
                       ),
                       context: context,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0)),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
                       backgroundColor: Colors.white,
                     ),
                   ),
